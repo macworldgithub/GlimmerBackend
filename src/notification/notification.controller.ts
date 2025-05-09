553092a74ec6d1fc404968b37bb775d4eee5bea2
hammad lodhi <hammadak05@gmail.com>
2025-05-09 13:08:21 +0500

    all notifications for admin

--- src/notification/notification.controller.ts
+++ src/notification/notification.controller.ts
@@ -1,15 +1,19 @@
 // notification.controller.ts
 
-import { Controller, Get, Param, Patch } from '@nestjs/common';
+import { Controller, Get, Param, Patch, Query } from '@nestjs/common';
 import { NotificationService } from './notification.service';
 
 @Controller('notifications')
 export class NotificationController {
   constructor(private readonly notificationService: NotificationService) {}
 
-  @Get(':userId')
-  async getNotifications(@Param('userId') userId: string) {
-    return this.notificationService.getNotificationsByUser(userId);
+  @Get()
+  async getAllNotifications(@Query('userId') userId?: string) {
+    if (userId) {
+      return this.notificationService.getNotificationsByUser(userId);
+    } else {
+      return this.notificationService.findAll();
+    }
   }
 
   @Patch(':id/read')
