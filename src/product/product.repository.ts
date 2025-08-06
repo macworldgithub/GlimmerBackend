9bcbacb9ade09b8f9be4a9596629768b79d61dc1
HUZAIFA <karimjawwad09@gmail.com>
2025-08-06 12:10:41 +0500

    Store products issue resolved

--- src/product/product.repository.ts
+++ src/product/product.repository.ts
@@ -52,7 +52,7 @@ export class ProductRepository {
       },
       projection,
     )
-    .sort({ created_at: -1 })
+.sort({ created_at: -1, _id: -1 })
     .skip(skip)
     .limit(DEFAULT_DOCUMENTS_LIMITS)
     .exec();
